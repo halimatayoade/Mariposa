0f9405c5d31df57604b1bc5174d9c1ea5e423e43
Halimat Ayoade <ayoadehal@gmail.com>
2021-06-04 14:37:52 -0500

    Adding more products to the database

--- src/Product.js
+++ src/Product.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 function Product(props) {
-
+  
   return (
     <div className="product">
       <h1>{props.product.name}</h1>
-      <img 
-        src={props.product.imageURL} 
-        alt={props.product.name} 
-      />
+      {
+        props.product.imageURL.map((char) => (
+          <img src={char} alt={props.product.name} />
+        ))
+      }
       <h2>{props.product.rating}/5</h2>
     </div>
     
