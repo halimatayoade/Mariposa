40719980c5df2860111b7badd7d21dbc9aeac8e7
Halimat Ayoade <ayoadehal@gmail.com>
2021-06-08 17:48:35 -0500

    Home page is finished

--- src/Product.js
+++ src/Product.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
 function Product(props) {
-  
   return (
-    <div className="product">
-      <ol><h1>{props.product.name}</h1></ol>
-      {
-        props.product.imageURL.map((char) => (
-          <img src={char} alt={props.product.name} />
-        ))
-      }
-      <h2>{props.product.rating}/5</h2>
+    <div class="product">
+      <img src={props.product.imageURL[0]} alt={props.product.name}/>
+      <p class="brand">{props.product.brand}</p>
+      <p class="name">{props.product.name}</p>
+      <p class="price">${props.product.price}</p>
+      <div data-toggled="true" class="listToggle">
+        <span id="heart" class="material-icons-outlined">
+          favorite_border
+        </span>
+      </div>
+      <p><button>Add to Cart</button></p>
     </div>
-    
   );
 }
 
