b6dfe1c42999dbe9b15ae6727b305c577b7f2428
Halimat Ayoade <ayoadehal@gmail.com>
2021-06-13 01:25:40 -0500

    resolving conflicts

--- src/Details.js
+++ src/Details.js
@@ -1,16 +1,23 @@
 import React, {useState} from 'react';
 import { useParams } from 'react-router-dom';
+import './product-pages.css';
 
 const Details = (props) => {
   const { id } = useParams();
   let item = props.allproducts.filter(data => data.id == id);
-  console.log(item)
-  console.log(item.name)
+
+
   return (
     <>
-      {
-       <h1>{item[0].name}</h1>
-      }
+      <div class="product-details">
+        {item[0].imageURL.map((char, index) => (
+          <img src={char}/>
+        ))}
+        <h1>{item[0].name}</h1>
+        <div className="stars restaurant-rating" style={{ "--rating": item[0].rating }}></div>
+        <p>{item[0].description}</p>
+        <p>{item[0].ingredients}</p>
+      </div>
     </>
   );
 }
